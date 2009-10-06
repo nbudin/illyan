66206bed95ee7637a723cb8e62f8580cfc6cc0d4
Nat Budin <natbudin@gmail.com>
2009-10-06 09:29:08 -0400

    An attempt to make shared models pluggable

--- rails/init.rb
+++ rails/init.rb
@@ -3,6 +3,7 @@
 require 'ae_users'
 
 ActiveRecord::Base.send(:include, AeUsers::Acts::Permissioned)
+ActiveRecord::Base.send(:include, AeUsers::Acts::SharedModel)
 ActionController::Base.send(:include, AeUsers::ControllerExtensions::RequirePermission)
 ActionView::Base.send(:include, AeUsers::HelperFunctions)
 ActionView::Base.send(:include, AeUsers::FormHelperFunctions)
