1e98213736e81343092a198ad4e750224a35dde4
Nat Budin <natbudin@gmail.com>
2010-04-26 17:18:38 -0400

    Clean up old and busted ae_users stuff

--- lib/illyan.rb
+++ lib/illyan.rb
@@ -132,6 +132,7 @@ module Illyan
 
     Acl9::config.merge!(
             :default_subject_class_name => "Person",
+            :default_subject_method => "current_person",
             :protect_global_roles => true
     )
   end
--- lib/illyan/controller_extensions.rb
+++ lib/illyan/controller_extensions.rb
@@ -4,91 +4,11 @@ module Illyan
       def self.included(base)
         base.class_eval do
           filter_parameter_logging :password, :password_confirmation
-          helper_method :person, :logged_in_person, :logged_in?
         end
       end
-      
-      private
-      
-      def person
-        current_person
-      end
-
-      alias_method :logged_in?, :person
-      alias_method :logged_in_person, :person
     end
     
     module RequirePermission
-      def self.included(base)
-        base.extend ClassMethods
-      end
-      
-      def create_account_and_person()
-        account = Account.new(:password => params[:password1])
-        person = Person.new(params[:person])
-        addr = EmailAddress.new :address => params[:email], :person => person, :primary => true
-        person.account = account
-        
-        if not Illyan.profile_class.nil?
-          app_profile = Illyan.profile_class.send(:new, :person => person)
-          app_profile.attributes = params[:app_profile]
-        end
-            
-        if request.post?
-          error_fields = []
-          error_messages = []
-        
-          if Person.find_by_email_address(params[:email])
-            error_fields.push "email"
-            error_messages.push "An account at that email address already exists!"
-          end
-        
-          if params[:password1] != params[:password2]
-            error_fields += ["password1", "password2"]
-            error_messages.push "Passwords do not match."
-          elsif params[:password1].length == 0
-            error_fields += ["password1", "password2"]
-            error_messages.push "You must enter a password."
-          end
-        
-          ["firstname", "lastname", "email", "gender"].each do |field|
-            if (not params[field] or params[field].length == 0) and (not params[:person][field] or params[:person][field].length == 0)
-              error_fields.push field
-              error_messages.push "You must enter a value for #{field}."
-            end
-          end
-          
-          if error_fields.size > 0 or error_messages.size > 0
-            flash[:error_fields] = error_fields
-            flash[:error_messages] = error_messages
-          else
-            account.save
-            addr.save
-            person.save
-            if app_profile
-              app_profile.save
-            end
-            
-            @account = account
-            @addr = addr
-            @person = person
-            @app_profile = app_profile
-        
-            begin
-              ActionMailer::Base.default_url_options[:host] = request.host
-              account.generate_activation
-            rescue
-              account.activation_key = nil
-              account.active = true
-              account.save
-              return :no_activation
-            end
-          
-            return :success
-          end
-        end
-      end
-
       module ClassMethods
         def require_login(conditions = {})
           code = <<-EOC
