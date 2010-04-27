9aa032863e436a81cb1495d4f4a076d20ec36c66
Nat Budin <natbudin@gmail.com>
2010-04-27 13:23:28 -0400

    Clean up dead code

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,35 +1,5 @@
 class AuthController < ApplicationController
   unloadable
-  filter_parameter_logging :password
-  before_filter :construct_login, :only => [:login, :openid_login, :forgot_form]
-  
-  def index
-    respond_to do |format|
-      format.css { render :layout => false }
-    end
-  end
-  
-  def openid_login
-    params[:openid_url] ||= cookies['openid_url']    
-    if using_open_id?
-      if attempt_open_id_login(@login.return_to)
-        successful_login_redirect
-      end
-    end
-  end
-  
-  def login
-    if request.post?
-      unless @login.password or @login.have_password
-        redirect_to :controller => "account", :action => "signup", :email => @login.email
-      end
-    end
-    if request.post? and not logged_in?
-      if attempt_login(@login)
-        successful_login_redirect
-      end
-    end
-  end
   
   def needs_person
     @open_id_identity = OpenIdIdentity.find_or_create_by_identity_url(session[:identity_url])
@@ -99,12 +69,6 @@ class AuthController < ApplicationController
       end
     end
   end
-
-  def auth_form  
-    respond_to do |format|
-      format.js { render :layout => false }
-    end
-  end
   
   def needs_profile
     @person = Person.find session[:provisional_person]
@@ -129,74 +93,6 @@ class AuthController < ApplicationController
           redirect_to params[:return_to]
         end
       end
-    end
-  end
-  
-  def forgot
-    ActionMailer::Base.default_url_options[:host] = request.host
-    
-    @account = Account.find_by_email_address(params[:email])
-    if not @account.nil?
-      @account.generate_password
-    else
-      flash[:error_messages] = ["There's no account matching that email address.  Please try again, or sign up for an account."]
-      redirect_to :action => :forgot_form
-    end
-  end
-  
-  def resend_validation
-    ActionMailer::Base.default_url_options[:host] = request.host
-    
-    @email_address = Account.find params[:email]
-    if not @email_address.nil?
-      @email_address.generate_validation
-    else
-      flash[:error_messages] = ["Email address #{params[:email]} not found!"]
-      redirect_to url_for("/")
-    end
-  end
-  
-  def logout
-    reset_session
-    redirect_to url_for("/")
-  end
-  
-  private
-  
-  def construct_login
-    @login = Login.new(params[:login])
-    @login.email ||= cookies['email']
-    if @login.return_to.nil? or @login.return_to == ""
-      if params[:return_to]
-        @login.return_to = params[:return_to]
-      else
-        @login.return_to = request.env["HTTP_REFERER"]
-      end
-    end
-
-    # prevent infinite redirect loops
-    begin
-      if URI(@login.return_to).path == URI(request.url).path
-        @login.return_to = url_for("/")
-      end
-    rescue
-    end
-    
-    # if they're already logged in, don't let them view this page
-    if logged_in?
-      successful_login_redirect
-    end
-  end
-  
-  def successful_login_redirect
-    if @login.return_to
-      redirect_to @login.return_to
-    elsif session[:return_to]
-      rt = session[:return_to]
-      session[:return_to] = nil
-      redirect_to rt
-    else
-      redirect_to url_for('/')
     end
   end
 end
