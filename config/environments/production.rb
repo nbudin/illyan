2cfa682dba1fcceb7fbe8ef3f8a35802f3111cbb
Nat Budin <natbudin@gmail.com>
2010-05-21 14:47:28 -0400

    Add default URL options for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Illyan::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
+  config.action_mailer.default_url_options = { :host => 'accounts.sugarpond.net' }
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
