72d835f02e4873124948039662e3a89b4a6ba0d9
Nat Budin <natbudin@gmail.com>
2011-03-28 11:50:27 -0400

    Get rid of legacy ae_users stuff, without deleting devise stuff

--- lib/illyan/form_builder_extensions.rb
+++
@@ -1,7 +0,0 @@
-module Illyan
-  module FormBuilderExtensions
-    def person_field(method, options = {})
-      @template.send("person_field", @object_name, method, objectify_options(options))
-    end
-  end
-end
