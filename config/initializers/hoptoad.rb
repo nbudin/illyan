3d3aa63643e0920f3abf34c0c24ad227655a3769
Nat Budin <natbudin@gmail.com>
2012-02-04 08:12:17 -0500

    More config vars

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,5 +1,7 @@
-if ENV['HOPTOAD_API_KEY']
+api_key = ENV['HOPTOAD_API_KEY'] || Illyan::Application.config_vars['hoptoad_api_key']
+
+if api_key
   HoptoadNotifier.configure do |config|
-    config.api_key = ENV['HOPTOAD_API_KEY']
+    config.api_key = api_key
   end
 end
