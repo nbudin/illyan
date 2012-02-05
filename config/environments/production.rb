aa72e0b882ca1b5106614fb8b344d31ca2f312a9
Nat Budin <natbudin@gmail.com>
2012-02-05 11:08:53 -0500

    Try local smtp instead

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,10 +38,7 @@ Illyan::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {
-    :arguments => '-i'
-  }
+  config.action_mailer.delivery_method = :smtp
 
   # Enable threaded mode
   # config.threadsafe!
