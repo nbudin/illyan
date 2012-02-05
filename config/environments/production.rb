805fb7432c2a3614174710fcabcc7713c96ecdbd
Nat Budin <natbudin@gmail.com>
2012-02-05 11:04:26 -0500

    Configure for exim

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,10 @@ Illyan::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = {
+    :arguments => '-i'
+  }
 
   # Enable threaded mode
   # config.threadsafe!
