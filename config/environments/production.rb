8027cbe40fdbed6d0db466f06037292b7e7f80a8
Nat Budin <natbudin@gmail.com>
2012-02-26 22:19:38 -0500

    SMTP settings for localhost

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,11 @@ Illyan::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port => 25,
+    :enable_starttls_auto => false
+  }
 
   # Enable threaded mode
   # config.threadsafe!
