7b7d8a7f75e354a8ec41fa05c1849fb117d312b1
Nat Budin <natbudin@gmail.com>
2012-05-02 11:45:25 -0400

    rvm_cmd still needs to be set

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :user, "www-data"
 set :domain, "#{user}@popper.sugarpond.net"
 set :repository, "git://github.com/nbudin/illyan.git"
 set :deploy_to, "/var/www/illyan"
-#set :rvm_cmd, "source /etc/profile.d/rvm.sh"
+set :rvm_cmd, nil #"source /etc/profile.d/rvm.sh"
 set :bundle_cmd, [ rvm_cmd, "env $(cat #{shared_path}/config/production.env) bundle" ].compact.join(" && ")
 
 set :scm, :git
