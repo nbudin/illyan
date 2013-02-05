91f80a0b49ab9c24418592ccee7de099d36f5c3f
Nat Budin <nbudin@patientslikeme.com>
2013-02-05 10:12:21 -0500

    Use Passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,14 +14,14 @@ set :scm, :git
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
 
 namespace :vlad do
   remote_task :copy_config_files, :roles => :app do
@@ -35,28 +35,6 @@ namespace :vlad do
   end
 end
 
-namespace :foreman do
-  desc "Export the Procfile to Ubuntu's upstart scripts"
-  remote_task :export, :roles => :app do
-    run "cd #{release_path} && sudo /var/www/update-foreman-conf '#{application}' '#{shared_path}'"
-  end
-
-  desc "Start the application services"
-  remote_task :start, :roles => :app do
-    sudo "start #{application}"
-  end
-
-  desc "Stop the application services"
-  remote_task :stop, :roles => :app do
-    sudo "stop #{application}"
-  end
-
-  desc "Restart the application services"
-  remote_task :restart, :roles => :app do
-    run "sudo start #{application} || sudo restart #{application}"
-  end
-end
-
 task "vlad:deploy" => %w[
-  vlad:update vlad:bundle:install vlad:copy_config_files foreman:export vlad:airbrake:deploy foreman:restart vlad:cleanup
+  vlad:update vlad:bundle:install vlad:copy_config_files vlad:airbrake:deploy deploy:restart vlad:cleanup
 ]
