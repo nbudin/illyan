8ecedc823354b4e0a4eef90342d6225bd4652508
Nat Budin <nbudin@patientslikeme.com>
2014-07-04 17:17:02 -0700

    iPhixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,16 +34,6 @@ module ApplicationHelper
     return title
   end
   
-  def site_banner
-    link_to root_path, class: "navbar-brand" do
-      if Illyan::Application.site_logo
-        image_tag(Illyan::Application.site_logo, :alt => Illyan::Application.site_title, :class => "img-responsive")
-      else
-        Illyan::Application.site_title
-      end
-    end
-  end
-  
   def theme_stylesheet_link_tag
     if Illyan::Application.theme
       stylesheet_link_tag Illyan::Application.theme
