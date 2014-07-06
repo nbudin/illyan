b1031e7524073814096ea1379fb5c2cd047aa1e4
Nat Budin <nbudin@patientslikeme.com>
2014-07-06 10:21:56 -0700

    Don't need the monkeypatch because we're overriding it in the controller

--- config/initializers/cassy.rb
+++ config/initializers/cassy.rb
@@ -1,9 +1,2 @@
 Cassy::Engine.config.config_file = File.expand_path("config/cassy.yml", Rails.root)
 require 'illyan/devise_session_authenticator'
-
-# Monkeypatch to make Cassy use Illyan's service model to look up valid services
-module Cassy::CAS
-  def valid_services
-    Service.pluck :url
-  end
-end
