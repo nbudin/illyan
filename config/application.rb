f05a5a9cf2ae8fea8cbeea3d5d38a0a0689a3623
Nat Budin <nbudin@patientslikeme.com>
2018-06-09 10:21:22 -0700

    Nicen up the user-facing views

--- config/application.rb
+++ config/application.rb
@@ -13,11 +13,11 @@ module Illyan
     def self.index_name
       "illyan_#{Rails.env}"
     end
-    
+
     config.assets.enabled = true
     config.assets.version = '1.0'
     config.assets.precompile += ["account-central.css", /\A\/liberation-serif\//]
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
