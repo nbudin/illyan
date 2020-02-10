aaccfe2e081e17c2b55be8a1603bc63d5be336d2
Nat Budin <natbudin@gmail.com>
2020-02-10 14:10:14 -0800

    rails isn't loaded yet

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' if Rails.env.development? || Rails.env.test? # Speed up boot time by caching expensive operations.
+if %w[development test].include?(ENV['RAILS_ENV'])
+  require 'bootsnap/setup'
+end
