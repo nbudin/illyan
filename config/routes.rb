5520f11203c42edf5bde1014019c150a947b8d52
Nat Budin <natbudin@gmail.com>
2020-02-10 12:19:38 -0800

    Rails 5.1 app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
-Illyan::Application.routes.draw do
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   use_doorkeeper
   resources :services
   devise_for :people
-  
+
   resource :profile do
     get :change_password
   end
-  
+
   namespace :admin do
     resources :people do
       member do
@@ -15,8 +17,8 @@ Illyan::Application.routes.draw do
       end
     end
   end
-  
+
   cassy controllers: { sessions: "sessions" }
-  
+
   root :to => "home#index"
 end
